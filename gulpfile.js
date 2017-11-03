52a7c6f7bbe402058d69e2d5505874d688f3423f
ko <ko.liu.learn@gmail.com>
2017-11-03 14:48:11 +0800

    Refactor and Integrate gulp-sourcemaps: map to css sources

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,33 @@ var gulp = require('gulp'); // 引入 gulp
 var $ = require('gulp-load-plugins')(); // just use for gulp-xxx
 var autoprefixer = require('autoprefixer');
 
+var srcs = {
+  html: './src/**/*.html',
+  jade: './src/**/*.jade',
+  scss: './src/scss/*.scss',
+  js: './src/js/**/*.js',
+};
+
+var dist = './dist/';
+
 // 加入任務
 gulp.task('copyHTML', function() {
   // copy src/*.* to dist/
-  return gulp.src('./src/**/*.html')
+  return gulp.src(srcs.html)
     .pipe($.plumber())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(dist));
 });
 
 gulp.task('jade', function() {
   // var YOUR_LOCALS = {};
 
   // gulp.src('./src/*.jade')
-  gulp.src('./src/**/*.jade') // for all sub-dirs & files belong to src
+  gulp.src(srcs.jade) // for all sub-dirs & files belong to src
     .pipe($.plumber())
     .pipe($.jade({
       pretty: true // Don't compress
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest(dist))
 });
 
 gulp.task('scss', function() {
@@ -34,29 +43,31 @@ gulp.task('scss', function() {
     })
   ];
 
-  return gulp.src('./src/scss/*.scss')
+  return gulp.src(srcs.scss)
+    .pipe($.sourcemaps.init())
     .pipe($.plumber())
     .pipe($.sass().on('error', $.sass.logError)) // compile to css
     .pipe($.postcss(plugins))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest(dist + 'css'));
 });
 
 gulp.task('babel', () =>
-  gulp.src('./src/js/**/*.js')
+  gulp.src(srcs.js)
   .pipe($.sourcemaps.init())
   .pipe($.babel({
     presets: ['es2015']
   }))
   .pipe($.concat('all.js'))
   .pipe($.sourcemaps.write('.'))
-  .pipe(gulp.dest('./dist/js'))
+  .pipe(gulp.dest(dist + 'js'))
 );
 
 // monitoring source changes & autorun the task
 gulp.task('watch', function() {
-  gulp.watch('./src/scss/*.scss', ['scss']);
-  gulp.watch('./src/**/*.jade', ['jade']);
-  gulp.watch('./src/js/**/*.js', ['babel']);
+  gulp.watch(srcs.scss, ['scss']);
+  gulp.watch(srcs.jade, ['jade']);
+  gulp.watch(srcs.js, ['babel']);
 });
 
 // default task
--- src/js/all2.js
+++ src/js/all2.js
@@ -1,2 +1,3 @@
 let fn2 = () => { console.log('a2') };
 fn2();
+fn();
